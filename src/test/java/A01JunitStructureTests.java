114887d070cbe14cce589c363a62e2fd8e3bf45c
lenartsz <72968750+lenartsz@users.noreply.github.com>
2025-10-12 10:21:14 +0200

    Add files via upload

--- src/test/java/A01JunitStructureTests.java
+++ src/test/java/A01JunitStructureTests.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.*;
 
 import static org.junit.Assert.assertTrue;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 //2. Check order of test Execution/TestMethodOrder
 public class A01JunitStructureTests {
     // Annotations
@@ -11,10 +12,42 @@ public class A01JunitStructureTests {
     // 5. Assertions with own error message
     // 6. mvn test / mvn surefire-report:report
 
+    @BeforeAll
+    public static void beforeAll() {
+        System.out.println("Before All");
+    }
 
+    @AfterAll
+    public static void afterAll() {
+        System.out.println("After All");
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.out.println("Test IS DONE");
+    }
+
+    @BeforeEach
+    void setUp() {
+        System.out.println("Test IS Running");
+    }
+
+    @Order(1)
     //First Test case
     @Test
     public void structureFirst() {
         System.out.println("In test First execution");
+        Assertions.assertTrue(false, "True is expected, but false arrived");
+    }
+    @Order(2)
+    @Disabled
+    @Test
+    public void structureSecond() {
+        System.out.println("In test Second execution");
+    }
+
+    @Test
+    public void structureThird() {
+        System.out.println("In test Third execution");
     }
 }
--- src/test/java/B01RestAssuredSimpleGETTests.java
+++ src/test/java/B01RestAssuredSimpleGETTests.java
@@ -1,11 +1,16 @@
 import io.restassured.RestAssured;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.specification.ResponseSpecification;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.http.Header;
+import io.restassured.http.Headers;
 
-import static io.restassured.RestAssured.baseURI;
-import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.*;
 import static io.restassured.config.SSLConfig.sslConfig;
-
+import io.restassured.builder.RequestSpecBuilder;
 public class B01RestAssuredSimpleGETTests {
 
     //Origin of the Structure: BDD (Behavior-Driven Development):
@@ -17,19 +22,81 @@ public class B01RestAssuredSimpleGETTests {
     // 0. https://restful-api.dev/
     // 1. log().all()
     // 2. Add "Accept" header with "application/text"
-    // 3  New test case
-    // 4. Add with pathParams given().pathParam("id", "1") + path
+    // 3  New test cases
+    // 4. Add with pathParams given().pathParam("id", "1") + path /{id}
     // 5. GET with queryParams given().queryParam("id","3").queryParam("id","4") NO path
+    // 6. BeforeAll RestAssured.config = RestAssured.config().sslConfig(sslConfig().relaxedHTTPSValidation()) + baseURI
+    /* 7. BeforeAll
+     RequestSpecification requestSpec = new RequestSpecBuilder()
+                .setBaseUri("https://api.restful-api.dev/objects")
+                .setRelaxedHTTPSValidation()
+                .addHeader("Accept","application/json").build();
 
-    // 6. BeforeAll RestAssured.config = RestAssured.config() + baseURI
+        RestAssured.requestSpecification = requestSpec;
+    /* 8.
+        ResponseSpecification globalResponseSpec = new ResponseSpecBuilder()
+                .expectStatusCode(200)
+                .log(LogDetail.ALL) // Log all response details
+                .build();
+
+        // Set it globally
+        RestAssured.responseSpecification = globalResponseSpec;
+    */
+
+    @BeforeAll
+    public static void setup() {
+        /*
+        RestAssured.config = RestAssured.config().sslConfig(sslConfig().relaxedHTTPSValidation());
+        baseURI = "https://api.restful-api.dev/objects";
+        */
+        RequestSpecification requestSpec = new RequestSpecBuilder()
+                .setBaseUri("https://api.restful-api.dev/objects")
+                .setRelaxedHTTPSValidation()
+                .addHeader("Accept","application/json").build();
+
+        RestAssured.requestSpecification = requestSpec;
+
+        ResponseSpecification globalResponseSpec = new ResponseSpecBuilder()
+                .expectStatusCode(200)
+                .log(LogDetail.ALL) // Log all response details
+                .build();
+
+        // Set it globally
+        RestAssured.responseSpecification = globalResponseSpec;
+
+    }
 
     @Test
     public void simpleGet() {
         given().relaxedHTTPSValidation()
-                .header("Content-Type", "application/json")
+                .header("Accept", "application/json")
                 .when()
                 .get("https://api.restful-api.dev/objects")
                 .then()
-                .statusCode(200).log().all();
+                .statusCode(200);
+    }
+
+    @Test
+    public void simpleGet1() {
+        given()
+                .when()
+                .get()
+                .then();
+    }
+
+    @Test
+    public void simpleGetPathParam() {
+        given().pathParam("id", "7")
+                .when()
+                .get("{id}")
+                .then();
+    }
+
+    @Test
+    public void simpleGetQueryParam() {
+        given().queryParam("id", "7","id","10")
+                .when()
+                .get()
+                .then();
     }
 }
