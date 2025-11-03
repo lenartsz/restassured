c990bbc172a7731cec7e44dae3a2506bdd7b1b0c
lenartsz <72968750+lenartsz@users.noreply.github.com>
2025-11-03 17:49:20 +0100

    Update B03RestAssuredComplexTests.java

--- src/test/java/B03RestAssuredComplexTests.java
+++ src/test/java/B03RestAssuredComplexTests.java
@@ -27,8 +27,7 @@ import io.restassured.matcher.RestAssuredMatchers.*;
 //0. Check https://restful-api.dev/
 //1. Create a new device with POST: given.body(requestBody)
 //   Use Response to access to response details: then().extract().response();
-//2. use global private static variables to store id: deviceId = response.path("id") and
-//   name
+//2. use global private static variables to store id: deviceId = response.path("id") and name
 //3. Assert name
 //4. Run get with param, using the generated id, use the same validation
 //5. Take care of the Order of the tests
